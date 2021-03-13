b09db82a225b60ba745e1816a41aceb762133571
Fatma Chaouech <fatmachaouech@insat.u-carthage.tn>
2021-03-13 12:23:43 +0100

    last commit

--- ex2/MiniWord.js
+++ ex2/MiniWord.js
@@ -1,8 +1,11 @@
 function changeFont(font) {
-    document.getElementById("note_header").style.fontFamily = font.value;
-  }
- function changeTextSize() {
-    var input = document.getElementById('input').value;
+  document.getElementById("note_header").style.fontFamily = font.value;
+}
+function changeTextSize() {
+  var input = document.getElementById('input').value;
 
-    document.getElementById("note_header").style.fontSize = input + "px";
-  }
+  document.getElementById("note_header").style.fontSize = input + "px";
+}
+function changeColor() {
+  document.getElementById("note_header").style.color = document.getElementById("couleur");
+}
