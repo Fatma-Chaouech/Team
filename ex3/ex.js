1bb1dff84941a049eedb33f0262ceb69cd9a198f
Fatma Chaouech <fatmachaouech@insat.u-carthage.tn>
2021-03-13 12:28:40 +0100

    full commit

---
+++ ex3/ex.js
@@ -0,0 +1,3 @@
+if((getElementById(jour).value)&&(getElementById(date);value))
+getElementById(jour).appendChild(getElementByI(injour).value);
+getElementById(tache).appendChild(getElementByI(intache).value);
