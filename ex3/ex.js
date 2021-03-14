fbd0e53fc1f21bc848b80324de3815815adc2943
Fatma Chaouech <fatmachaouech@insat.u-carthage.tn>
2021-03-15 00:13:29 +0100

    full commit 2.0

--- ex3/ex.js
+++ ex3/ex.js
@@ -1,3 +1,54 @@
-if((getElementById(jour).value)&&(getElementById(date);value))
-getElementById(jour).appendChild(getElementByI(injour).value);
-getElementById(tache).appendChild(getElementByI(intache).value);
+
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue1 = document.getElementById("myInput1").value;
+  var inputValue2 =document.getElementById("myInput2").value;
+  var t1 = document.createTextNode(inputValue1);
+  var t2 = document.createTextNode(inputValue2);
+  li.appendChild(t1);
+  li.appendChild(t2);
+  if ((inputValue1 === '')||(inputValue2 === '')){
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+
+  document.getElementById("myInput1").value = "";
+  document.getElementById("myInput2").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
